9762ab0949dde44c787f31b2773c81eb2d218194
AlphaINF <876066505@qq.com>
2023-03-18 16:41:59 +0800

    support multithreading

--- judgement-analyzer/extract_judges.py
+++ judgement-analyzer/extract_judges.py
@@ -1,6 +1,5 @@
 import os
-from tqdm import tqdm
-import concurrent.futures
+from multiprocessing import Pool, cpu_count
 
 def filter_lines_with_keywords(input_filename, output_filename, keyword1, keyword2):
     total_lines = 0
@@ -18,19 +17,21 @@ def filter_lines_with_keywords(input_filename, output_filename, keyword1, keywor
                     output_file.write(line)
     return input_filename, total_lines, count
 
-def process_file(input_file, input_folder, output_folder, keyword1, keyword2):
+def process_file(args):
+    input_file, input_folder, output_folder, keyword1, keyword2 = args
     input_filename = os.path.join(input_folder, input_file)
     output_filename = os.path.join(output_folder, input_file)
     return filter_lines_with_keywords(input_filename, output_filename, keyword1, keyword2)
 
-def process_files(input_folder, output_folder, keyword1, keyword2, num_threads=4):
+def process_files(input_folder, output_folder, keyword1, keyword2):
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
     input_files = [f for f in os.listdir(input_folder) if os.path.isfile(os.path.join(input_folder, f))]
+    num_processes = cpu_count()
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
-        results = list(executor.map(process_file, input_files, [input_folder]*len(input_files), [output_folder]*len(input_files), [keyword1]*len(input_files), [keyword2]*len(input_files)), total=len(input_files))
+    with Pool(processes=num_processes) as pool:
+        results = pool.map(process_file, [(input_file, input_folder, output_folder, keyword1, keyword2) for input_file in input_files])
 
     for result in results:
         input_file, total_lines, count = result
@@ -39,9 +40,8 @@ def process_files(input_folder, output_folder, keyword1, keyword2, num_threads=4
         print(f"  包含 '{keyword1}' 和 '{keyword2}' 的行数为: {count}")
 
 if __name__ == "__main__":
-    input_folder = "./judge_input - 副本"  # 请将此处替换为你的输入文件夹路径
-    output_folder = "./judge_output_副本"  # 请将此处替换为你的输出文件夹路径
+    input_folder = "./judge_input"  # 请将此处替换为你的输入文件夹路径
+    output_folder = "./judge_output"  # 请将此处替换为你的输出文件夹路径
     keyword1 = '"劳动争议"'
-    keyword2 = '判决书"",'
-    num_threads = 4
-    process_files(input_folder, output_folder, keyword1, keyword2, num_threads)
+    keyword2 = '判决书""'
+    process_files(input_folder, output_folder, keyword1, keyword2)
